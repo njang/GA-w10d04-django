b3fcfcda979a490b5c526ec3ab967e2933766283
njang <neo.w.jang@gmail.com>
2018-02-15 10:57:00 -0600

    Show hard-coded data on index.html

--- main_app/views.py
+++ main_app/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'index.html')
-	# return HttpResponse('<h1>Hello Explorers!</h1>')
+	return render(request, 'index.html', {'treasures': treasures})
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creed, NM"),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+    Treasure('Coffee Can', 20.00, 'tin', "Acme, CA")
+]
