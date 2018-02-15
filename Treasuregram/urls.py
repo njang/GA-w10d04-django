98502ff31dbdd49040ad77a299f8930ceab564a2
njang <neo.w.jang@gmail.com>
2018-02-15 10:55:29 -0600

    Add index.html

--- Treasuregram/urls.py
+++ Treasuregram/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
-from main_app import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path(r'', include('main_app.urls'))
 ]
