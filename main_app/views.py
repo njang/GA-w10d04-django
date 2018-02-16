a1e9189f4ad22275509d969c1be7fa1ac605c4d0
njang <neo.w.jang@gmail.com>
2018-02-15 23:37:32 -0600

    Complete up to step 3 of README2

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Treasure
+from .forms import TreasureForm
 
 def index(request):
 	treasures = Treasure.objects.all()
@@ -10,6 +11,17 @@ def show(request, treasure_id):
 	treasure = Treasure.objects.get(id=treasure_id)
 	return render(request, 'show.html', {'treasure': treasure}) 
 
+def post_treasure(request):
+    form = TreasureForm(request.POST)
+    if form.is_valid():
+        treasure = Treasure(
+            name=form.cleaned_data['name'],
+            value=form.cleaned_data['value'],
+            material=form.cleaned_data['material'],
+            location=form.cleaned_data['location'])
+        treasure.save()
+    return HttpResponseRedirect('/')
+    
 # class Treasure:
 #     def __init__(self, name, value, material, location):
 #         self.name = name
