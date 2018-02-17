fb0f8cf0fd95a248a218b4c960a698d7602bd3ad
njang <neo.w.jang@gmail.com>
2018-02-16 23:52:52 -0600

    Enable main_app routing

--- Treasuregram/urls.py
+++ Treasuregram/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.conf.urls import url
-from django.urls import path
-from main_app import views
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'admin/', admin.site.urls),
-    url(r'', views.index),
+    path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
 ]
