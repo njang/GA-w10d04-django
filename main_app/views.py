e9bc1a7a8ba7fc6c87768a5e476ae914e750aa3a
njang <neo.w.jang@gmail.com>
2018-02-17 13:53:46 -0600

    Refactor code before adding user functionalities

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Treasure
 from .forms import TreasureForm
 
@@ -15,24 +15,5 @@ def show(request, treasure_id):
 def post_treasure(request):
     form = TreasureForm(request.POST)
     if form.is_valid():
-        # treasure = Treasure(
-        #     name=form.cleaned_data['name'],
-        #     value=form.cleaned_data['value'],
-        #     material=form.cleaned_data['material'],
-        #     location=form.cleaned_data['location'])
-        # treasure.save()
         form.save(commit = True)
     return HttpResponseRedirect('/')
-
-# class Treasure:
-#     def __init__(self, name, value, material, location):
-#         self.name = name
-#         self.value = value
-#         self.material = material
-#         self.location = location
-
-# treasures = [
-#     Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creed, NM"),
-#     Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
-#     Treasure('Coffee Can', 20.00, 'tin', "Acme, CA")
-# ]
