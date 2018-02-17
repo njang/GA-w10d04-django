c0c20028d9cfbb01cd70425ce6bd0f8d4bb13764
njang <neo.w.jang@gmail.com>
2018-02-16 18:52:28 -0600

    Deploy on heroku

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Treasure(models.Model):
@@ -6,6 +7,6 @@ class Treasure(models.Model):
 	value = models.DecimalField(max_digits=10, decimal_places=2)
 	material = models.CharField(max_length=100)
 	location = models.CharField(max_length=100)
-
+	# user = models.ForeignKey(User)
 	def __str__(self):
 		return self.name
